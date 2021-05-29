d73ece52a959089111a47684df2f7b55a4161784
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-05-29 20:56:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from pyrogram import Client, filters
 from googletrans import Translator
 
 FayasNoushad = Client(
-    "Translator Bot"
+    "Translator Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
 )
 
 TRANSLATOR = Translator()
