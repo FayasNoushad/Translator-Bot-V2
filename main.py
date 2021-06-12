3e177652851ff5e9cbc392ad1d8f11b9cb27314c
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-12 14:55:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # (C) @FayasNoushad
 # Copyright permission under MIT License
 # All rights reserved by FayasNoushad
-# License -> https://github.com/FayasNoushad/Translator-Bot/blob/main/LICENSE
+# License -> https://github.com/FayasNoushad/Translator-Bot-V2/blob/main/LICENSE
 
 import os
 from pyrogram import Client, filters
